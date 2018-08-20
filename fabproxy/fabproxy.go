de2919be5dec19877668f5ca93a39bcaf52c6f1e
Swetha Repakula <srepaku@us.ibm.com>
2018-08-20 13:01:11 -0700

    [FAB-11080] MVP Fabproxy
      - fabproxy implments minimum set of JSON RPC API calls to deploy and
      interact with EVM bytecode smart contracts
      - `getCode`,`call`,`sendTransaction`,`accounts`,`getTransactionReceipt`
    
    Change-Id: I34b29b40e5a81d1ce23506aeeb49d9bc962cdd5f
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

---
+++ fabproxy/fabproxy.go
@@ -0,0 +1,39 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package fabproxy
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc/v2"
+)
+
+type FabProxy struct {
+	server *rpc.Server
+}
+
+func NewFabProxy(service EthService) *FabProxy {
+	server := rpc.NewServer()
+
+	proxy := &FabProxy{
+		server: server,
+	}
+
+	server.RegisterCodec(NewRPCCodec(), "application/json")
+	server.RegisterService(service, "eth")
+
+	return proxy
+}
+
+func (p *FabProxy) Start(port int) {
+	r := mux.NewRouter()
+	r.Handle("/", p.server)
+
+	http.ListenAndServe(fmt.Sprintf(":%d", port), r)
+}
