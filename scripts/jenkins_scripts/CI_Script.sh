a42a15d7c92847300e9b0dfc4e5a80811fd268f3
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-10-05 16:47:01 -0400

    FAB-12326 Add function to install nodejs
    Add function to install nodejs in Jenkins pipeline before
    run integration tests. Call this function where ever it is required.
    
    Change-Id: I8c4ec541dbd7e1ab1addee9f9de7adec7329749a
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- scripts/jenkins_scripts/CI_Script.sh
+++ scripts/jenkins_scripts/CI_Script.sh
@@ -7,6 +7,8 @@
 
 # exit on first error
 
+export NODE_VER=8.11.3 # Default nodejs version
+
 Parse_Arguments() {
       while [ $# -gt 0 ]; do
               case $1 in
@@ -15,6 +17,9 @@ Parse_Arguments() {
                             ;;
                       --clean_Environment)
                             clean_Environment
+                            ;;
+                      --install_Node)
+                            install_Node
                             ;;
               esac
               shift
@@ -64,6 +69,26 @@ rm -rf gopath/src/github.com/hyperledger/fabric-ca/vendor/github.com/cloudflare/
 
 clearContainers
 removeUnwantedImages
+}
+
+install_Node() {
+
+        # Install nvm to install multi node versions
+        wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+        # shellcheck source=/dev/null
+        export NVM_DIR="$HOME/.nvm"
+        # shellcheck source=/dev/null
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+        echo "------> Install NodeJS"
+        # This also depends on the fabric-baseimage. Make sure you modify there as well.
+        echo "------> Use $NODE_VER for >=release-1.1 branches"
+        nvm install $NODE_VER || true
+        # use nodejs version
+        nvm use --delete-prefix v$NODE_VER --silent
+
+        echo "npm version ------> $(npm -v)"
+        echo "node version ------> $(node -v)"
 }
 
 env_Info() {
