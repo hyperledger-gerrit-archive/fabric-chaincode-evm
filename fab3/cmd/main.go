7f4212fe5eb1e5bb4a2f293459d80eb8535e847a
Swetha Repakula <srepaku@us.ibm.com>
2019-01-22 17:44:35 -0800

    [FAB-13065] Fab3 exits gracefully on exit
     - Fab3 will now shutdown gracefully when it receives the interrupt
     signal
    
    Change-Id: I94bbf743b28f5730802bd522229d61edbcc73949
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- fab3/cmd/main.go
+++ fab3/cmd/main.go
@@ -9,6 +9,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 
 	"go.uber.org/zap"
 
@@ -98,6 +99,7 @@ func checkFlags() error {
 }
 
 // Runs Fab3
+// Will exit gracefully for errors and signal interrupts
 func runFab3(cmd *cobra.Command, args []string) error {
 	sdk, err := fabsdk.New(config.FromFile(cfg))
 	if err != nil {
@@ -126,7 +128,27 @@ func runFab3(cmd *cobra.Command, args []string) error {
 
 	logger.Infof("Starting Fab3 on port %d", port)
 	proxy := fab3.NewFab3(ethService)
-	return proxy.Start(port)
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- proxy.Start(port)
+	}()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	select {
+	case err = <-errChan:
+	case <-signalChan:
+		err = proxy.Shutdown()
+	}
+
+	if err != nil {
+		logger.Infof("Fab3 has exited with an error: %s", err)
+		return err
+	}
+	logger.Info("Fab3 has exited")
+	return nil
 }
 
 func main() {
