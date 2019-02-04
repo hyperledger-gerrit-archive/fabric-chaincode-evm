7907ce59531607469d0487bcfe751dbcfe5f4a86
Morgan Bauer <mbauer@us.ibm.com>
2019-02-04 14:13:57 -0800

    FAB-14034 vendor counterfeiter
     - specific makefile target to build
     - ran update-mocks
     - remove outdated golint target
     - go get update so our tools evolve over time
     - fix dep dependency so it tries to build
    
    Change-Id: I3560e5c701bce6c6453a5618c9fc7592df623282
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>

--- gotools.mk
+++ gotools.mk
@@ -14,8 +14,7 @@ go.fqp.counterfeiter := github.com/maxbrunsfeld/counterfeiter
 go.fqp.gocov         := github.com/axw/gocov/gocov
 go.fqp.gocov-xml     := github.com/AlekSi/gocov-xml
 go.fqp.goimports     := golang.org/x/tools/cmd/goimports
-#-----temporarty workaround for FAB-9667---
-#go.fqp.golint        := github.com/golang/lint/golint
+go.fqp.golint        := github.com/golang/lint/golint
 go.fqp.misspell      := github.com/client9/misspell/cmd/misspell
 go.fqp.mockery       := github.com/vektra/mockery/cmd/mockery
 go.fqp.manifest-tool       := github.com/estesp/manifest-tool
@@ -26,11 +25,6 @@ gotools-install: $(patsubst %,$(GOTOOLS_BINDIR)/%, $(GOTOOLS))
 .PHONY: gotools-clean
 gotools-clean:
 	-@rm -rf $(BUILD_DIR)/gotools
-
-#-----temporarty workaround for FAB-9667---
-gotool.golint:
-	@git clone https://github.com/golang/lint.git $(GOTOOLS_GOPATH)/src/golang.org/x/lint
-	@GOPATH=$(abspath $(GOTOOLS_GOPATH)) GOBIN=$(abspath $(GOTOOLS_BINDIR)) go install golang.org/x/lint/golint
 
 # Special override for ginkgo since we want to use the version vendored with the project
 gotool.ginkgo:
@@ -46,11 +40,14 @@ gotool.dep:
 	@GOPATH=$(abspath $(GOTOOLS_GOPATH)) GOBIN=$(abspath $(GOTOOLS_BINDIR)) go install -ldflags="-X main.version=$(DEP_VERSION) -X main.buildDate=$$(date '+%Y-%m-%d')" github.com/golang/dep/cmd/dep
 	@git -C $(abspath $(GOTOOLS_GOPATH))/src/github.com/golang/dep checkout -q master
 
+gotool.counterfeiter:
+	 go install ./vendor/github.com/maxbrunsfeld/counterfeiter
+
 # Default rule for gotools uses the name->path map for a generic 'go get' style build
 gotool.%:
 	$(eval TOOL = ${subst gotool.,,${@}})
 	@echo "Building ${go.fqp.${TOOL}} -> $(TOOL)"
-	@GOPATH=$(abspath $(GOTOOLS_GOPATH)) GOBIN=$(abspath $(GOTOOLS_BINDIR)) go get ${go.fqp.${TOOL}}
+	@GOPATH=$(abspath $(GOTOOLS_GOPATH)) GOBIN=$(abspath $(GOTOOLS_BINDIR)) go get -u ${go.fqp.${TOOL}}
 
 $(GOTOOLS_BINDIR)/%:
 	$(eval TOOL = ${subst $(GOTOOLS_BINDIR)/,,${@}})
