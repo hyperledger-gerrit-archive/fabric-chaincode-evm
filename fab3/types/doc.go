0374915a6af8e3ba8277b329807695b0eb5444b5
Morgan Bauer <mbauer@us.ibm.com>
2019-02-12 15:12:55 -0800

    FAB-14164 move input and output structs to fab3/types
    Change-Id: Ibd07045daf32e33c27f8212e9385784f2b735db5
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>

---
+++ fab3/types/doc.go
@@ -0,0 +1,12 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+/*
+Package types contains the types used to interact with the json-rpc
+interface. It exists for users of fab3 types to use them without importing the
+fabric protobuf definitions.
+*/
+package types
