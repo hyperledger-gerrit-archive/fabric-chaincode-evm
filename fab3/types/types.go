e3c68211b0b8c2401e1137b719a24fb10b7b38b9
Morgan Bauer <mbauer@us.ibm.com>
2019-02-12 15:12:55 -0800

    FAB-14164 move TxReceipt and Log to fab3/types
    Change-Id: Ibd07045daf32e33c27f8212e9385784f2b735db5
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>

---
+++ fab3/types/types.go
@@ -0,0 +1,36 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+/*
+Package types contains the types used to interact with the json-rpc
+interface. It exists for users of fab3 types to use them without importing the
+fabric protobuf definitions.
+*/
+package types
+
+type TxReceipt struct {
+	TransactionHash   string `json:"transactionHash"`
+	TransactionIndex  string `json:"transactionIndex"`
+	BlockHash         string `json:"blockHash"`
+	BlockNumber       string `json:"blockNumber"`
+	ContractAddress   string `json:"contractAddress"`
+	GasUsed           int    `json:"gasUsed"`
+	CumulativeGasUsed int    `json:"cumulativeGasUsed"`
+	To                string `json:"to"`
+	Logs              []Log  `json:"logs"`
+	Status            string `json:"status"`
+}
+
+type Log struct {
+	Address     string   `json:"address"`
+	Topics      []string `json:"topics"`
+	Data        string   `json:"data"`
+	BlockNumber string   `json:"blockNumber"`
+	TxHash      string   `json:"transactionHash"`
+	TxIndex     string   `json:"transactionIndex"`
+	BlockHash   string   `json:"blockHash"`
+	Index       string   `json:"logIndex"`
+}
