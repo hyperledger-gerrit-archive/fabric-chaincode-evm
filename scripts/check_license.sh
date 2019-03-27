e3385117c6b51d352661b143e16453344ff61bca
rameshthoomu <rameshbabu.thoomu@gmail.com>
2019-03-27 00:26:18 -0400

    FAB-14868 Update pipeline scripts
    This patch updates the existing pipeline scripts to use
    global shared library and removes the redundant code from
    this repo.
    
    Also, optimized Makfile and added debug statements.
    
    Change-Id: I1098a4828143ec6b677867bd863e26123e274877
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -30,6 +30,7 @@ function filterExcludedFiles {
 		| grep -v "^Gopkg\.lock$" \
 		| grep -v "\.md$" \
 		| grep -v "\.pb\.go$" \
+		| grep -v "ci.properties$" \
 		| sort -u`
 
   CHECK=$(filterGeneratedFiles "$CHECK")
